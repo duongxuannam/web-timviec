b6ddcfdc22804cf835cb8d5f33555c2c1cc7ae47
Duong Xuan Nam <yduongxuannam1995@gmail.com>
2018-01-23 16:54:11 +0700

    aa

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import  Product from './Component/Product/Product';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+      
+      <nav class="navbar navbar-inverse">
+        <a class="navbar-brand" >Title</a>
+        <ul class="nav navbar-nav">
+          <li class="active">
+            <a >Home</a>
+          </li>
+          <li>
+            <a >Link</a>
+          </li>
+        </ul>
+      </nav>
+      
+      <div class="container">
+        
+        <div class="row">
+          <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <Product/>
+            <Product/> <Product/> <Product/> <Product/> <Product/> <Product/> <Product/> <Product/>
+          </div>
+        </div>
+        
       </div>
+      
+       </div>
     );
   }
 }
---
+++ src/Component/Product/Product.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class Product extends Component {
+    render() {
+        return (
+            <div>
+                <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                    <div class="thumbnail">
+                        <img  alt=""/>
+                        <div class="caption">
+                            <h3>Ten San Pham</h3>
+                            <p>
+                               5.000.000 VND
+                            </p>
+                            <p>
+                                <a href="" class="btn btn-primary">Mua ngay</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                
+                </div>
+        );
+    }
+}
+export default Product;
